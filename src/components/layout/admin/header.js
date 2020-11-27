9de24c58092758bb877620f830135dbb767898da
Bruno Kuchel <bruno.kuchel@al.infnet.edu.br>
2020-11-27 14:56:08 +0100

    lista de likes e dislikes no profile restô

--- src/components/layout/admin/header.js
+++ src/components/layout/admin/header.js
@@ -120,7 +120,7 @@ const Header = styled.div`
     }
 
     .navbar-nav .nav-link{
-        color: #1b314c;
+        color: #212529;
     }
 
    
@@ -130,6 +130,9 @@ const Logo = styled.div`
     font-size: 1.8em;
     display:flex;
     align-items: center;
+    span { 
+        color: #212529;
+    }
 `
 const NavUser = styled.div`
     text-align: right;
